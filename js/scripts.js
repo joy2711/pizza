0756b2f9e4da970d61b0ad6417c5dc74e965d1a5
joy2711 <laishakamau9@gmail.com>
2020-03-03 10:08:20 +0300

    add initial page structure

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
+
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quan").value;
+    return parseInt(selectedQuantity);
+}
+
+function getTotalAmount() {
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
+    alert("You have Ordered" + getQuantity("") + " pizza." + "" + " The Total Amount is kshs " + (totalAmount) + "" + " Thank you for your order welcome again.");
+    prompt("enter your location")
+    alert("You delivery fee is 200/=")
+    alert("You order will be delivered in a few")
+    alert("welcome back.")
+}
